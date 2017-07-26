358d31f22c39f64b4c4d4e5a389bb0619e92f101
ElmasSayed <elmassayed@gmail.com>
2017-07-26 02:54:02 -0700

    seach and categories working

--- logic.js
+++ logic.js
@@ -1,6 +1,5 @@
 // http://api.eventful.com/docs
 var apiKey = "SS64KtgmxNNMgC94";
-
 // search button clicked
 $("#searchBtn").click(function() {
     console.log("search btn clicked")
@@ -19,9 +18,7 @@ $("#searchBtn").click(function() {
     }).done(function(data) {
         console.log(data);
         // console.log(JSON.stringify(data, null, 4));
-
         // Transfer content to html
-
         // $("#img").html(data.events.event[0].image.thumb.url);
         $("#title").html(data.events.event[0].title);
         $("#date").html(data.events.event[0].start_time);
@@ -33,22 +30,16 @@ $("#searchBtn").click(function() {
         $("#geocode").html(data.events.event[0].geocode_type);
         $("#venue_url").html(data.events.event[0].venue_url);
     });
-
 }); //search button click event end
-
-
 // ___________________________________________________________________________________________________________________________
 // category selected
-$("#categories").on("click", ".category", function(event) {
-    var category = $(this).text();
 
+$("#categories li").click(function() {
+    var category = this.id;
     console.log('Clicked category: ' + category);
-
     var searchText = $("#searchText").val();
-
-    var queryURL = "https://api.eventful.com/json/events/search?" + category + "&location=" + searchText + "&date=Future?&app_key=SS64KtgmxNNMgC94"
+    var queryURL = "https://api.eventful.com/json/events/search?category=" + category + "&location=" + searchText + "&date=Future?&app_key=SS64KtgmxNNMgC94"
     console.log("queryURL = " + queryURL);
-
     $.ajax({
         url: queryURL,
         method: "GET",
@@ -57,15 +48,3 @@ $("#categories").on("click", ".category", function(event) {
         console.log(data);
     });
 });
-
-// $('.category').click(function(event) {
-//     var category = $('#categories').val($(this).html());
-
-// val($(this).html()
-// console.log('clicked');
-
-// var queryURL = "http://api.eventful.com/json/events/search?app_key=" + apiKey + "&location=" + searchText + "&date=Future?";
-// console.log("queryURL = " + queryURL);
-
-// var queryURL = "http://api.eventful.com/json/events/search?app_key=" + apiKey + "&location=" + searchText + "&date=Future?";
-// console.log("queryURL = " + queryURL);
