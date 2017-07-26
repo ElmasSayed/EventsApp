3235c6f08b708c477b3b770c5e11b4d7706eb129
ElmasSayed <elmassayed@gmail.com>
2017-07-25 20:39:20 -0700

    first

---
+++ logic.js
@@ -0,0 +1,71 @@
+// http://api.eventful.com/docs
+var apiKey = "SS64KtgmxNNMgC94";
+
+// search button clicked
+$("#searchBtn").click(function() {
+    console.log("search btn clicked")
+    var searchText = $("#searchText").val();
+    console.log("searchText = " + searchText);
+
+    var queryURL = "http://api.eventful.com/json/events/search?app_key=" + apiKey + "&location=" + searchText + "&date=Future?";
+    // console.log("queryURL = " + queryURL);
+
+    // AJAX Function
+    // location
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        dataType: "jsonp"
+    }).done(function(data) {
+        console.log(data);
+        // console.log(JSON.stringify(data, null, 4));
+
+        // Transfer content to html
+
+        // $("#img").html(data.events.event[0].image.thumb.url);
+        $("#title").html(data.events.event[0].title);
+        $("#date").html(data.events.event[0].start_time);
+        $("#description").html(data.events.event[0].description);
+        $("#eventUrl").html(data.events.event[0].url);
+        $("#venue").html(data.events.event[0].venue_name);
+        $("#venueAdd").html(data.events.event[0].venue_address);
+        $("#cityName").html(data.events.event[0].city_name);
+        $("#geocode").html(data.events.event[0].geocode_type);
+        $("#venue_url").html(data.events.event[0].venue_url);
+    });
+
+}); //search button click event end
+
+
+// ___________________________________________________________________________________________________________________________
+// category selected
+$("#categories").on("click", ".category", function(event) {
+    var category = $(this).text();
+
+    console.log('Clicked category: ' + category);
+
+    var searchText = $("#searchText").val();
+
+    var queryURL = "https://api.eventful.com/json/events/search?" + category + "&location=" + searchText + "&date=Future?&app_key=SS64KtgmxNNMgC94"
+    console.log("queryURL = " + queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        dataType: "jsonp"
+    }).done(function(data) {
+        console.log(data);
+    });
+});
+
+// $('.category').click(function(event) {
+//     var category = $('#categories').val($(this).html());
+
+// val($(this).html()
+// console.log('clicked');
+
+// var queryURL = "http://api.eventful.com/json/events/search?app_key=" + apiKey + "&location=" + searchText + "&date=Future?";
+// console.log("queryURL = " + queryURL);
+
+// var queryURL = "http://api.eventful.com/json/events/search?app_key=" + apiKey + "&location=" + searchText + "&date=Future?";
+// console.log("queryURL = " + queryURL);
